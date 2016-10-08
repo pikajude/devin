48e99462f65b4a022c04b82949e0961c3bfc4d0f
Jude Taylor <me@jude.bio>
2016-10-07 17:47:05 -0700

    remove ixset-typed

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 { mkDerivation, ansi-wl-pprint, attoparsec, base, bytestring
 , containers, damnpacket, exceptions, hostname, html-entities
-, io-streams, irc, ixset-typed, lens, lifted-async, lifted-base
-, logging-effect, machines, monad-control, mtl, network, resourcet
-, safe-exceptions, stdenv, stm, template-haskell, text, time
-, transformers-base, unordered-containers, utf8-string
+, io-streams, irc, lens, lifted-async, lifted-base, logging-effect
+, monad-control, mtl, network, resourcet, safe-exceptions, stdenv
+, stm, template-haskell, text, time, transformers-base
+, unbounded-delays, unordered-containers, utf8-string
 }:
 mkDerivation {
   pname = "devin";
@@ -13,10 +13,10 @@ mkDerivation {
   isExecutable = true;
   executableHaskellDepends = [
     ansi-wl-pprint attoparsec base bytestring containers damnpacket
-    exceptions hostname html-entities io-streams irc ixset-typed lens
-    lifted-async lifted-base logging-effect machines monad-control mtl
-    network resourcet safe-exceptions stm template-haskell text time
-    transformers-base unordered-containers utf8-string
+    exceptions hostname html-entities io-streams irc lens lifted-async
+    lifted-base logging-effect monad-control mtl network resourcet
+    safe-exceptions stm template-haskell text time transformers-base
+    unbounded-delays unordered-containers utf8-string
   ];
   homepage = "https://github.com/pikajude/devin";
   description = "What melvin \"What kevin should have been\" should have been";
